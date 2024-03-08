7360bd51c5be08c8189e45a2baa3f97d7a2d11f2
Edenilson Souza <edenilson.sza@gmail.com>
2024-03-08 14:15:13 -0300

    fix: corrige input de pesquisa e componente de select

--- src/app/views/Filters.tsx
+++ src/app/views/Filters.tsx
@@ -2,6 +2,7 @@ import Button from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { FaSearch } from "react-icons/fa";
 import { Select } from "../../components/ui/select";
+import { useState } from "react";
 
 type FiltersProps = {
     setFilters: (e: any) => void;
@@ -12,6 +13,7 @@ type FiltersProps = {
 };
 
 export function Filters({ setFilters, options }: FiltersProps) {
+    const [selectedFilter, setSelectedFilter] = useState(options[0].value);
     const handleSearch = () => {
         const searchFilter = document.getElementById("searchFilter") as HTMLSelectElement;
         const searchInput = document.getElementById("searchInput") as HTMLInputElement;
@@ -23,14 +25,14 @@ export function Filters({ setFilters, options }: FiltersProps) {
             <div className='flex flex-row h-auto'>
                 <div className='w-full h-20 flex flex-col justify-center items-center'>
                     <div className='flex justify-between items-center '>
-                        <Select className='w-12/1 min-w-32 rounded pl-2 border-gray-100'>
+                        <Select className='w-12/1 min-w-32 rounded pl-2 border-gray-100' id='searchFilter'>
                             {options.map((option: any) => (
                                 <option key={option.value} value={option.value}>
                                     {option.label}
                                 </option>
                             ))}
                         </Select>
-                        <Input type='text' id='searchInput' className='text-gray-800  ml-2 mr-4 min-w-32' name='email' placeholder='Pesquisar...' />
+                        <Input type='text' id='searchInput' name='searchInput' className='text-gray-800  ml-2 mr-4 min-w-32' placeholder='Pesquisar...' />
                         <Button
                             className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex flex-row items-center  mr-4'
                             onClick={() => handleSearch()}
