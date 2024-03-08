ff3484a636a710d9dce48ca2d8b4b7ac59278de6
Edenilson Souza <edenilson.sza@gmail.com>
2024-03-08 14:15:45 -0300

    style: remove comentario

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -8,7 +8,6 @@ interface TextProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export const Select: React.FC<TextProps> = ({ children, element = "select", className, ...rest }) => {
-    // export function Select({ children, element = "select", className, ...rest }: TextProps) {
     const Component = element;
     const baseStyled =
         "w-1/3 h-10 rounded border border-zinc-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-0";
